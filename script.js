ff8f56a9b403cb3701d8c1e927ead64a1a6e35cd
Francesco Peluso <francesco.peluso04@gmail.com>
2021-05-08 02:00:14 +0200

    First page redefined - DON'T TOUCH

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-function loginPressed()
+function buyerLog()
 {
-  var old_menu = document.getElementById("welcome-menu");
-  var new_menu = document.getElementById("login-menu");
+  var old_menu = document.getElementById("sell-or-buy");
+  var new_menu = document.getElementById("register-buyer");
   old_menu.classList.add("hide");
   new_menu.classList.remove("hide")
 }
 
-
-function registerPressed()
+function sellerLog()
 {
-  var old_menu = document.getElementById("welcome-menu")
-  var new_menu = document.getElementById("register-menu")
+  var old_menu = document.getElementById("sell-or-buy")
+  var new_menu = document.getElementById("register-buyer")
   old_menu.classList.add("hide");
   new_menu.classList.remove("hide")
+}
+
+function resetIndex() 
+{
+  main_menu = document.getElementById("sell-or-buy")
+  main_menu.classList.remove("hide")
+
+  other_menus = [
+    document.getElementById("register-buyer"),
+    document.getElementById("register-seller")
+  ];
+
+  for (var i = 0; i < other_menus.length; i++) {
+    other_menus[i].classList.add("hide");
+  }
+
 }
