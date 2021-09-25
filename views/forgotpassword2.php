c63961e2756f1c32da245e888ba577ef19817fd7
Francesco Peluso <59664699+thatsfrankie@users.noreply.github.com>
2021-09-25 07:34:35 +0200

    BETA RELEASE of IOcommerce - Fixes

--- views/forgotpassword2.php
+++ views/forgotpassword2.php
@@ -13,7 +13,7 @@
     <title>Password reset - IOcommerce</title>
     <link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/login-register-reset.css">
+    <link rel="stylesheet" href="css/login-reset.css">
 </head>
 <body>
     <nav>
